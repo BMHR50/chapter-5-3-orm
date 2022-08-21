397b2095b598abda8f7412d23f15eab1d54c62e4
BenaDasBInIch <bena.invest101@gmail.com>
2022-08-21 14:42:52 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express')
+//const morgan = require('morgan')
+
+// import data
+
+// import controllers
+//const menu_router = require('./controllers/menu_controller')
+//const admin_router = require('./controllers/admin_controller')
+const auth_router = require('./controllers/auth_controller')
+//const order_router = require('./controllers/order_controller')
+
+// init express App
+const app = express()
+
+// server info
+const host = 'localhost'
+const port = 3000
+
+// init/assign middleware
+//app.use(morgan('combined'))
+app.use(express.json())
+
+// list endpoints
+app.get('/', function (req, res){
+    res.json({
+        host: host,
+        port: port
+    })
+})
+
+// init routes
+//app.use('/menu', menu_router)
+//app.use('/admin', admin_router)
+//app.use('/customer/order', order_router)
+app.use(auth_router)
+
+// contoh init handler yang menggunakan class
+app.listen(port, host, () => {
+    console.log(`server running on: http://${host}:${port}`)
+})
+
